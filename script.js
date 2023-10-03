2e2fa16a3a8e9cf7e32520a07b604d4ddf703f5c
unknown <tedd.franck@gmail.com>
2023-10-03 14:01:23 +0200

    mon portfolio

--- script.js
+++ script.js
@@ -6,19 +6,16 @@ menuToggle.addEventListener('click', () => {
 });
 
 
-// Fonction pour activer le mode sombre
 function enableDarkMode() {
     document.body.classList.add('dark-mode');
     localStorage.setItem('dark-mode', 'enabled');
 }
 
-// Fonction pour désactiver le mode sombre
 function disableDarkMode() {
     document.body.classList.remove('dark-mode');
     localStorage.setItem('dark-mode', 'disabled');
 }
 
-// Fonction pour basculer entre le mode sombre et le mode clair
 function toggleDarkMode() {
     if (localStorage.getItem('dark-mode') === 'enabled') {
         disableDarkMode();
@@ -27,14 +24,12 @@ function toggleDarkMode() {
     }
 }
 
-// Écouteur d'événement pour le bouton de commutation du mode sombre
 const darkModeToggle = document.getElementById('dark-mode-switch');
 darkModeToggle.addEventListener('change', toggleDarkMode);
 
-// Vérifie le mode sombre actuel au chargement de la page
 if (localStorage.getItem('dark-mode') === 'enabled') {
     enableDarkMode();
-    darkModeToggle.checked = true; // Coche le bouton du mode sombre si le mode sombre est activé
+    darkModeToggle.checked = true;
 } else {
     disableDarkMode();
 }
